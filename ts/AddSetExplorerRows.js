dbd3d338ce2e6821c242e725dce438554cd2f5e1
autofloccinaucinihilipilificator <125834149+autofloccinaucinihilipilificator@users.noreply.github.com>
2024-05-26 00:27:16 -0400

    Made set manager item buttons only appear for sets that actually exist

--- ts/AddSetExplorerRows.js
+++ ts/AddSetExplorerRows.js
@@ -5,15 +5,6 @@ setList.innerHTML = '';
 for (let i = 0; i < 128; i++) {
     setList.insertAdjacentHTML('beforeend', `<tr style="background-color: var(--set-background-color-${i % 2 == 1 ? 2 : 1});">
     <td class="set-col"><button type="button"></button></td>
-    <td class="button-col">
-        <div class="set-buttons">
-            <div class="delete-set" title="Delete set"><div></div></div>
-            <div class="edit-set" title="Edit set"><div><div></div></div></div>
-            <div class="set-arrows">
-                <div class="set-up-arrow" title="Move set up"><div></div></div>
-                <div class="set-down-arrow" title="Move set down"><div></div></div>
-            </div>
-        </div>
-    </td>
+    <td class="button-col"></td>
 </tr>`);
 }
